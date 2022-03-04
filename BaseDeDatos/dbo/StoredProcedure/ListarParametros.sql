c800a2029d6038c35d5d5783b7c5a3cb41ca4fda
ERICK ANDRES JIMENEZ <ejimenezb0179@uvirtual.ac.cr>
2022-03-03 18:41:34 -0600

    SP

---
+++ BaseDeDatos/dbo/StoredProcedure/ListarParametros.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[ListarParametros]
+ @Id_Parametro INT=NULL
+
+AS BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+			Id_Parametro
+		,   Codigo
+		,   Descripcion
+		,	Valor
+		,	Estado
+
+		FROM Parametros 
+
+		WHERE
+	     (@Id_Parametro IS NULL OR Id_Parametro=@Id_Parametro)
+END
