5aa49635787cf6704f36972208d3510874a2756e
ERICK ANDRES JIMENEZ <ejimenezb0179@uvirtual.ac.cr>
2022-03-03 20:47:27 -0600

    cambio

--- WebApp/Pages/Parametros/Grid.cshtml.cs
+++ WebApp/Pages/Parametros/Grid.cshtml.cs
@@ -2,15 +2,72 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using WBL;
 namespace WebApp.Pages.Parametros
 {
     public class GridModel : PageModel
     {
-        public void OnGet()
+        private readonly IParametrosService parametrosServices;
+
+        public GridModel(IParametrosService proveedorService)
         {
+            this.parametrosServices = proveedorService;
+        }
+
+        public IEnumerable<ParametrosEntity> GridList { get; set; } = new List<ParametrosEntity>();
+
+        public string Mensaje { get; set; } = "";
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                GridList = await parametrosServices.Get();
+
+                if (TempData.ContainsKey("Msg"))
+                {
+                    Mensaje = TempData["Msg"] as string;
+                }
+
+                TempData.Clear();
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+        }
+
+
+        public async Task<IActionResult> OnGetEliminar(int id)
+        {
+            try
+            {
+                var result = await parametrosServices.Delete(new()
+                {
+                    Id_Parametro = id
+                }
+                );
+
+                if (result.CodeError != 0)
+                {
+                    throw new Exception(result.MsgError);
+                }
+
+                TempData["Msg"] = "El registro se elimino correctamente";
+
+                return Redirect("Grid");
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
         }
     }
 }
